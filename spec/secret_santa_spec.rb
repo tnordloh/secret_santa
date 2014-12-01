6a6227c975649771ea59768ade94697de0271514
Timothy Nordloh <tnordloh@gmail.com>
2014-12-01 11:59:28 -0700

    add test for lib/secret_santa.rb

--- spec/secret_santa_spec.rb
+++ spec/secret_santa_spec.rb
@@ -3,6 +3,9 @@ require "minitest/autorun"
 require_relative "../lib/secret_santa"
 
 describe SecretSanta do 
-  data=File.readlines("./data/test1")
-  p SecretSanta.get_santas(data)
+  it "tests secret santa return" do
+    data=File.readlines("./data/test1")
+    p SecretSanta.get_santas(data)[0] 
+    (SecretSanta.get_santas(data)[0] =~ /luke/).must_equal(16)
+  end
 end
